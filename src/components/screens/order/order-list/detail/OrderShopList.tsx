5ea8b466bec1404dde02cb22a033a11108c76081
Tri Nguyen <tringuyen.moa@gmail.com>
2023-03-20 01:33:16 +0700

    feature/ new view manage PAGE

--- src/components/screens/order/order-list/detail/OrderShopList.tsx
+++ src/components/screens/order/order-list/detail/OrderShopList.tsx
@@ -1,11 +1,13 @@
-import { List, Typography } from 'antd'
+import { Typography } from 'antd'
 import { useRouter } from 'next/dist/client/router'
 import Link from 'next/link'
-import React, { FC } from 'react'
+import React, { FC, useMemo } from 'react'
 import { order } from '~/api'
 import { ActionButton } from '~/components/globals/button/ActionButton'
+import { DataTable } from '~/components/globals/table'
 import { toast } from '~/components/toast'
-import { numberWithCommas } from '~/utils'
+import { TColumnsType } from '~/types/table'
+import { _format } from '~/utils'
 
 type OrderIDShopListProps = {
 	data?: TOrder
@@ -25,56 +27,76 @@ export const OrderShopList: FC<OrderIDShopListProps> = ({ data, RoleID, refetch 
 			toast.error(error)
 		}
 	}
-
-	const renderView = () => {
-		if (!!data) {
-			return (
-				<List
-					size="small"
-					bordered
-					dataSource={data.SubMainOrders || []}
-					renderItem={(item, index) => (
-						<List.Item
-							style={{
-								paddingTop: 0,
-								paddingBottom: 0
+	const columns: TColumnsType<any> = useMemo(() => {
+		return [
+			{
+				dataIndex: 'MainOrderCustomID',
+				title: 'Mã đơn nhỏ'
+				// render: (id) => <Typography.Text mark>{id}</Typography.Text>
+			},
+			{
+				dataIndex: 'ProductQuantity',
+				title: 'Số lượng sản phẩm',
+				align: 'right',
+				width: 150
+			},
+			{
+				dataIndex: 'TotalPriceVND',
+				title: 'Tổng tiền (VNĐ)',
+				align: 'right',
+				width: 250,
+				render: (money) => _format.getVND(money, '')
+			},
+			{
+				dataIndex: 'Id',
+				title: 'Thao tác',
+				align: 'right',
+				render: (id, record) => (
+					<div>
+						<Link
+							href={{
+								pathname: '/manager/order/order-list/shopOrderDetail',
+								query: {
+									id: orderId,
+									shopOrderId: record?.Id
+								}
 							}}
-							key={`MainOrderCodes-${index}`}
-							actions={[
-								<Link
-									href={{
-										pathname: '/manager/order/order-list/shopOrderDetail',
-										query: {
-											id: orderId,
-											shopOrderId: item?.Id
-										}
-									}}
-									passHref
-								>
-									<a rel="noopener noreferrer">
-										<ActionButton
-											iconContainerClassName="ml-2 border-none"
-											icon="fas fa-info-square"
-											title="Chi tiết"
-											placement="right"
-										/>
-									</a>
-								</Link>
-							]}
+							passHref
 						>
-							<Typography.Text mark>{item?.MainOrderCustomID}</Typography.Text>
-							<Typography.Text>{item?.ProductQuantity}</Typography.Text>
-							<Typography.Text>{numberWithCommas(item?.TotalPriceVND)}</Typography.Text>
-						</List.Item>
-					)}
-				/>
-			)
-		} else return <>Loading...</>
+							<a rel="noopener noreferrer">
+								<ActionButton
+									iconContainerClassName="ml-2 border-none"
+									icon="fas fa-info-square"
+									title="Đi tới chi tiết"
+									placement="right"
+								/>
+							</a>
+						</Link>
+					</div>
+				)
+			}
+		]
+	}, [])
+	const renderTalbes = () => {
+		return (
+			<DataTable
+				{...{
+					style: 'secondary',
+					className: 'no-bgcolor-tablerow',
+					columns: columns,
+					data: data.SubMainOrders
+
+					// expandable: expandable
+				}}
+			/>
+		)
 	}
+
 	return (
 		<React.Fragment>
 			<div>
-				<div className="h-full">{renderView()}</div>
+				{/* <div className="h-full">{renderView()}</div> */}
+				<div className="p-4">{renderTalbes()}</div>
 			</div>
 		</React.Fragment>
 	)
